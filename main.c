e64cad5c2bd0e0d37d8aabb26682dae909d6c5d2
Johan Malm <jgm323@gmail.com>
2023-04-20 22:56:07 +0100

    Fix deprecated-declarations warning
    ...relating to G_APPLICATION_FLAGS_NONE

--- main.c
+++ main.c
@@ -325,7 +325,7 @@ main(int argc, char **argv)
 	/* start ui */
 	GtkApplication *app;
 	int status;
-	app = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new(NULL, G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
