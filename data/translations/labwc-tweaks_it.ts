e98ad4b623aa232907a28ea33936b34cced3b17a
redtide <redtid3@gmail.com>
2024-04-10 01:01:48 +0200

    Resources: translations, appstream metainfo, desktop file update

---
+++ data/translations/labwc-tweaks_it.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="30"/>
+        <source>Appearance</source>
+        <translation>Aspetto</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="36"/>
+        <source>Corner Radius</source>
+        <translation>Raggio Angolo</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="43"/>
+        <source>Openbox Theme</source>
+        <translation>Tema Openbox</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation>Mouse e Touchpad</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="63"/>
+        <source>Cursor Theme</source>
+        <translation>Tema Puntatore</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
+        <source>Cursor Size</source>
+        <translation>Grandezza Puntatore</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="83"/>
+        <source>Natural Scroll</source>
+        <translation>Scorrimento Naturale</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="94"/>
+        <source>Language &amp; Region</source>
+        <translation>Lingua e Paese</translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
+        <source>Keyboard Layout</source>
+        <translation>Mappatura Tastiera</translation>
+    </message>
+</context>
+</TS>
