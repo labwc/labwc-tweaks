07b46a39886e582caad90d30aec44614dfff03fa
redtide <redtid3@gmail.com>
2024-04-09 10:32:08 +0200

    Qt: replaced QTabWidget with QListWidget and QStackedWidget

--- tweaks-qt/main.cpp
+++ tweaks-qt/main.cpp
@@ -4,8 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    app.setApplicationName(QStringLiteral("labwc-tweaks"));
+
     MainDialog w;
     w.show();
-    return a.exec();
+
+    return app.exec();
 }
