ae7c416bbe9144cbe85b92bde55a43ed7f291ced
Weblate <noreply@weblate.org>
2024-04-14 12:58:58 +0200

    Weblate commits
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/it/
    Translation: Labwc/Labwc Tweaks Menu Entry

---
+++ data/translations/labwc-tweaks_nl.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="30"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="36"/>
+        <source>Corner Radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="43"/>
+        <source>Openbox Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="63"/>
+        <source>Cursor Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
+        <source>Cursor Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="83"/>
+        <source>Natural Scroll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="94"/>
+        <source>Language &amp; Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
+        <source>Keyboard Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
