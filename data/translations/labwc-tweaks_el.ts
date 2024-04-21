934d834e10296c03a5437cc69a1d5d547176511f
Weblate <noreply@weblate.org>
2024-04-21 10:23:43 +0200

    Weblate commits
    Co-authored-by: Abdullah Albaroty <albaroty@gmail.com>
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Sal Monella <someusersmightfindinconvenient@proton.me>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yannis Drougas <drougas@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/el/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ro/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ru/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

--- data/translations/labwc-tweaks_el.ts
+++ data/translations/labwc-tweaks_el.ts
@@ -1,52 +1,52 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ro">
+<TS version="2.1" language="el">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="30"/>
         <source>Appearance</source>
-        <translation>Aspect</translation>
+        <translation>Εμφάνιση</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="36"/>
         <source>Corner Radius</source>
-        <translation>Raza colțului</translation>
+        <translation>Γωνιακή ακτίνα</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="43"/>
         <source>Openbox Theme</source>
-        <translation>Tema Openbox</translation>
+        <translation>Θέμα Openbox</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>Mouse și tastatură</translation>
+        <translation>Ποντίκι &amp; επιφάνεια αφής</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="63"/>
         <source>Cursor Theme</source>
-        <translation>Temă Cursor</translation>
+        <translation>Θέμα δρομέα</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
         <source>Cursor Size</source>
-        <translation>Dimensiune Cursor</translation>
+        <translation>Μέγεθος δρομέα</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="83"/>
         <source>Natural Scroll</source>
-        <translation>Derulare Naturală</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="94"/>
         <source>Language &amp; Region</source>
-        <translation>Limbă și Regiune</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
         <source>Keyboard Layout</source>
-        <translation>Aspect tastatură</translation>
+        <translation>Διάταξη πληκτρολογίου</translation>
     </message>
 </context>
 </TS>
