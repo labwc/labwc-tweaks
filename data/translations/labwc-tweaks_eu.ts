237e73396d11cbb3469647aef59363fd63230f01
Weblate <noreply@weblate.org>
2024-05-04 10:34:03 +0200

    Weblate commits
    Co-authored-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/ru/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/es/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/it/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ru/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

--- data/translations/labwc-tweaks_eu.ts
+++ data/translations/labwc-tweaks_eu.ts
@@ -1,62 +1,62 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt">
+<TS version="2.1" language="eu">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>Aparência</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="110"/>
         <source>Corner Radius</source>
-        <translation>Raio do canto</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
         <source>Openbox Theme</source>
-        <translation>Tema Openbox</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="65"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>Rato e Painel tátil</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="136"/>
         <source>Cursor Theme</source>
-        <translation>Tema do cursor</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="146"/>
         <source>Cursor Size</source>
-        <translation>Tamanho do cursor</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="156"/>
         <source>Natural Scroll</source>
-        <translation>Deslocação natural</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="197"/>
         <source>Add</source>
-        <translation>Adicionar</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="204"/>
         <source>Remove</source>
-        <translation>Remover</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
         <source>Language &amp; Region</source>
-        <translation>Idioma e Região</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="182"/>
         <source>Keyboard Layout</source>
-        <translation>Esquema de teclado</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
