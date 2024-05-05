0c3809923ccc3eb3f9e15eac98050080f86cbddb
Weblate <noreply@weblate.org>
2024-05-05 14:59:03 +0200

    Weblate commits
    Co-authored-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/es/
    Translation: Labwc/Labwc Tweaks

--- data/translations/labwc-tweaks_et.ts
+++ data/translations/labwc-tweaks_et.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ca">
+<TS version="2.1" language="et">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>Aspecte</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="110"/>
         <source>Corner Radius</source>
-        <translation>Radi de cantonada</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
         <source>Openbox Theme</source>
-        <translation>Tema de l’Openbox</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="65"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>Ratolí i ratolí tàctil</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="136"/>
         <source>Cursor Theme</source>
-        <translation>Tema dels cursors</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="146"/>
         <source>Cursor Size</source>
-        <translation>Mida dels cursors</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="156"/>
         <source>Natural Scroll</source>
-        <translation>Desplaçament natural</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="197"/>
         <source>Add</source>
-        <translation>Afegeix</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="204"/>
         <source>Remove</source>
-        <translation>Elimina</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
         <source>Language &amp; Region</source>
-        <translation>Llengua i regió</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="182"/>
