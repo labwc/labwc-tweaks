2561113adf41a6726f7b0d7062d336e64883bfbc
Weblate <noreply@weblate.org>
2024-05-16 20:43:02 +0200

    Weblate commits
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: xe1st <dnzkckali@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/el/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/tr/
    Translation: Labwc/Labwc Tweaks

--- data/translations/labwc-tweaks_tr.ts
+++ data/translations/labwc-tweaks_tr.ts
@@ -1,62 +1,62 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="el">
+<TS version="2.1" language="tr">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>Εμφάνιση</translation>
+        <translation>Görünüş</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="110"/>
         <source>Corner Radius</source>
-        <translation>Γωνιακή ακτίνα</translation>
+        <translation>Köşe Yuvarlatma</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
         <source>Openbox Theme</source>
-        <translation>Θέμα Openbox</translation>
+        <translation>Openbox Teması</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="65"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>Ποντίκι &amp; επιφάνεια αφής</translation>
+        <translation>Fare ve Dokunmatik Yüzey</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="136"/>
         <source>Cursor Theme</source>
-        <translation>Θέμα δρομέα</translation>
+        <translation>İmleç Teması</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="146"/>
         <source>Cursor Size</source>
-        <translation>Μέγεθος δρομέα</translation>
+        <translation>İmleç Boyutu</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="156"/>
         <source>Natural Scroll</source>
-        <translation type="unfinished"></translation>
+        <translation>Doğal Kaydırma</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="197"/>
         <source>Add</source>
-        <translation>Προσθήκη</translation>
+        <translation>Ekle</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="204"/>
         <source>Remove</source>
-        <translation>Αφαίρεση</translation>
+        <translation>Kaldır</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
         <source>Language &amp; Region</source>
-        <translation type="unfinished"></translation>
+        <translation>Dil ve Bölge</translation>
     </message>
     <message>
         <location filename="../../tweaks-qt/maindialog.ui" line="182"/>
         <source>Keyboard Layout</source>
-        <translation>Διάταξη πληκτρολογίου</translation>
+        <translation>Klavye Düzeni</translation>
     </message>
 </context>
 </TS>
