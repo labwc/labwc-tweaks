9778037db29fda079c6d2c2320ce7196b5bd74db
stefonarch <standreas@riseup.net>
2024-05-19 20:18:05 +0200

    Updated *.ts files

--- data/translations/labwc-tweaks_et.ts
+++ data/translations/labwc-tweaks_et.ts
@@ -4,57 +4,72 @@
 <context>
     <name>MainDialog</name>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
+        <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
         <translation>Välimus</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="110"/>
+        <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
         <translation>Nurga raadius</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
+        <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
         <translation>Openbox&apos;i teema</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="65"/>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
         <translation>Hiir ja puuteplaat</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="136"/>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
         <translation>Kursori teema</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="146"/>
+        <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
         <translation>Kursori suurus</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="156"/>
+        <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
         <translation>Loomulik kerimine</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="197"/>
+        <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
         <translation>Lisa</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="204"/>
+        <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
         <translation>Eemalda</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
+        <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
         <translation>Keel ja regioon</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="182"/>
+        <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
         <translation>Klaviatuuri paigutus</translation>
     </message>
--- data/translations/labwc-tweaks_pl.ts
+++ data/translations/labwc-tweaks_pl.ts
@@ -4,57 +4,72 @@
 <context>
     <name>MainDialog</name>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="57"/>
+        <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
         <translation>Wygląd</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="110"/>
+        <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
         <translation>Promień narożnika</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="100"/>
+        <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
         <translation>Motyw Openboksa</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="65"/>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
         <translation>Mysz i panel dotykowy</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="136"/>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
         <translation>Motyw kursora</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="146"/>
+        <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
         <translation>Rozmiar kursora</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="156"/>
+        <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
         <translation>Przewijanie naturalne</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="197"/>
+        <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
         <translation>Dodaj</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="204"/>
+        <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
         <translation>Usuń</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="73"/>
+        <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
         <translation>Język i region</translation>
     </message>
     <message>
-        <location filename="../../tweaks-qt/maindialog.ui" line="182"/>
+        <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
         <translation>Układ klawiatury</translation>
     </message>
