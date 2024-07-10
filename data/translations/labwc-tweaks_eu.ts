675dc62513b994bb8a6a6e1c0ec6106348284ce3
Weblate <noreply@weblate.org>
2024-07-11 01:24:12 +0200

    Weblate commits
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/eu/
    Translation: Labwc/Labwc Tweaks

--- data/translations/labwc-tweaks_eu.ts
+++ data/translations/labwc-tweaks_eu.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Itxura</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
-        <translation type="unfinished"></translation>
+        <translation>Izkinako erradioa</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Openbox Gaia</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="65"/>
         <source>Behaviour</source>
-        <translation type="unfinished"></translation>
+        <translation>Portaera</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation type="unfinished"></translation>
+        <translation>Sagua eta Ukipen-panela</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="128"/>
@@ -36,17 +36,17 @@
     <message>
         <location filename="../../src/maindialog.ui" line="154"/>
         <source>Placement Policy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kokapen-politika</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Kurtsore Gaia</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Kurtsorearen tamaina</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="200"/>
@@ -56,12 +56,12 @@
     <message>
         <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Gehitu</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Kendu</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="81"/>
@@ -71,7 +71,7 @@
     <message>
         <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Teklatuaren Diseinua</translation>
     </message>
 </context>
 </TS>
