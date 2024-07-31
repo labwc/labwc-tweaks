183ed288b77245ea749c4d8c4c62496a8d4acdcb
Weblate <noreply@weblate.org>
2024-07-31 16:24:19 +0200

    Weblate commits
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Co-authored-by: Giannis Antypas <gianni.antypas@gmail.com>
    Co-authored-by: Jiri Grönroos <jiri.gronroos@iki.fi>
    Co-authored-by: Stars1233 <stars3211999@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/de/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/el/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/de/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/el/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/fi/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

--- data/translations/labwc-tweaks_en_IE.ts
+++ data/translations/labwc-tweaks_en_IE.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fi">
+<TS version="2.1" language="en_IE">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>Ulkoasu</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
-        <translation>Kulman säde</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
-        <translation>Openbox-teema</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="65"/>
         <source>Behaviour</source>
-        <translation>Toiminta</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>Hiiri &amp; kosketuslevy</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="128"/>
@@ -36,42 +36,42 @@
     <message>
         <location filename="../../src/maindialog.ui" line="154"/>
         <source>Placement Policy</source>
-        <translation>Sijoituskäytäntö</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
-        <translation>Osoitinteema</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
-        <translation>Osoittimen koko</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
-        <translation>Luonnollinen vieritys</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
-        <translation>Lisää</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
-        <translation>Poista</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
-        <translation>Kieli &amp; alue</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
-        <translation>Näppäimistöasettelu</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
