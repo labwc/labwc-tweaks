3181374481638b0ba8a5e37d94b0bfd8884ca3bb
Weblate <noreply@weblate.org>
2024-08-27 14:24:30 +0200

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: daniel <punished.haddock@proton.me>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/pt_BR/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

--- data/translations/labwc-tweaks_ko.ts
+++ data/translations/labwc-tweaks_ko.ts
@@ -1,77 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pa">
+<TS version="2.1" language="ko">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>ਦਿੱਖ</translation>
+        <translation>모양새</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
-        <translation>ਕੋਨਾ ਦੇ ਵਿਆਸ</translation>
+        <translation>모서리 반지름</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
-        <translation>ਓਪਨਬਾਕਸ ਥੀਮ</translation>
+        <translation>Openbox 테마</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="65"/>
         <source>Behaviour</source>
-        <translation>ਰਵੱਈਆ</translation>
+        <translation>동작</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>ਮਾਊਸ ਤੇ ਟੱਚਪੈਡ</translation>
+        <translation>마우스 &amp; 터치패드</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="128"/>
         <source>Drop Shadows</source>
-        <translation type="unfinished"></translation>
+        <translation>그림자</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="154"/>
         <source>Placement Policy</source>
-        <translation>ਸਥਾਪਨ ਨੀਤੀ</translation>
+        <translation>배치 방침</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
-        <translation>ਕਰਸਰ ਥੀਮ</translation>
+        <translation>커서 테마</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
-        <translation>ਕਰਸਰ ਦਾ ਆਕਾਰ</translation>
+        <translation>커서 크기</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
-        <translation>ਕੁਦਰਤੀ ਸਕਰੋਲ</translation>
+        <translation>부드러운 스크롤</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
-        <translation>ਜੋੜੋ</translation>
+        <translation>추가</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
-        <translation>ਹਟਾਓ</translation>
+        <translation>제거</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
-        <translation>ਭਾਸ਼ਾ ਤੇ ਖੇਤਰ</translation>
+        <translation>언어 &amp; 지역</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
-        <translation>ਕੀਬੋਰਡ ਖਾਕਾ</translation>
+        <translation>키보드 자판</translation>
     </message>
 </context>
 </TS>
--- data/translations/labwc-tweaks_pt_BR.ts
+++ data/translations/labwc-tweaks_pt_BR.ts
@@ -1,77 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pa">
+<TS version="2.1" language="pt_BR">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>ਦਿੱਖ</translation>
+        <translation>Aparência</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
-        <translation>ਕੋਨਾ ਦੇ ਵਿਆਸ</translation>
+        <translation>Arrendondamento dos Cantos</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
-        <translation>ਓਪਨਬਾਕਸ ਥੀਮ</translation>
+        <translation>Tema do Openbox</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="65"/>
         <source>Behaviour</source>
-        <translation>ਰਵੱਈਆ</translation>
+        <translation>Comportamento</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>ਮਾਊਸ ਤੇ ਟੱਚਪੈਡ</translation>
+        <translation>Mouse &amp; Touchpad</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="128"/>
         <source>Drop Shadows</source>
-        <translation type="unfinished"></translation>
+        <translation>Sombreamento nas Janelas</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="154"/>
         <source>Placement Policy</source>
-        <translation>ਸਥਾਪਨ ਨੀਤੀ</translation>
+        <translation>Política de Posicionamento</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
-        <translation>ਕਰਸਰ ਥੀਮ</translation>
+        <translation>Tema do Cursor</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
-        <translation>ਕਰਸਰ ਦਾ ਆਕਾਰ</translation>
+        <translation>Tamanho do Cursor</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
-        <translation>ਕੁਦਰਤੀ ਸਕਰੋਲ</translation>
+        <translation>Rolagem Natural</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
-        <translation>ਜੋੜੋ</translation>
+        <translation>Adicionar</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
-        <translation>ਹਟਾਓ</translation>
+        <translation>Remover</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
-        <translation>ਭਾਸ਼ਾ ਤੇ ਖੇਤਰ</translation>
+        <translation>Idioma &amp; Região</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
-        <translation>ਕੀਬੋਰਡ ਖਾਕਾ</translation>
+        <translation>Idioma do Teclado</translation>
     </message>
 </context>
 </TS>
