2a20d5896cd86fc4677f34acd302e4b2deae521a
Weblate <noreply@weblate.org>
2025-01-12 07:25:21 +0100

    Weblate commits
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: vTT <vttfreebsd@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ka/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

--- data/translations/labwc-tweaks_ka.ts
+++ data/translations/labwc-tweaks_ka.ts
@@ -1,77 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<TS version="2.1" language="ka">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>Apparence</translation>
+        <translation>გარეგნობა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
-        <translation>Rayon des coins</translation>
+        <translation>კუთხის რადიუსი</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
-        <translation>Thème Openbox</translation>
+        <translation>Openbox-ის თემა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="65"/>
         <source>Behaviour</source>
-        <translation>Comportement</translation>
+        <translation>ქცევა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>Souris &amp; Pavé tactile</translation>
+        <translation>თაგუნა და თაჩპედი</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="128"/>
         <source>Drop Shadows</source>
-        <translation>Contour ombré</translation>
+        <translation>ჩრდილები</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="154"/>
         <source>Placement Policy</source>
-        <translation>Politique de placement</translation>
+        <translation>მოთავსების პოლიტიკა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
-        <translation>Thème curseur</translation>
+        <translation>კურსორის თემა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
-        <translation>Taille curseur</translation>
+        <translation>კურსორის ზომა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
-        <translation>Défilement naturel</translation>
+        <translation>ბუნებრივი გადახვევა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
-        <translation>Ajouter</translation>
+        <translation>დამატება</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
-        <translation>Enlever</translation>
+        <translation>წაშლა</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
-        <translation>Langue &amp; Région</translation>
+        <translation>ენა და რეგიონი</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
-        <translation>Disposition du clavier</translation>
+        <translation>კლავიატურის განლაგება</translation>
     </message>
 </context>
 </TS>
