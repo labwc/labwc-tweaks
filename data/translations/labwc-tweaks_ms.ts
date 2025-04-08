7fbaa17d3b20796e815ac267d3ff51a0a663b26f
Weblate <noreply@weblate.org>
2025-04-08 08:25:52 +0200

    Weblate commits
    Co-authored-by: Qayyum Yazid <purrnama@proton.me>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/ms/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ms/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

---
+++ data/translations/labwc-tweaks_ms.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ms">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../src/maindialog.ui" line="57"/>
+        <source>Appearance</source>
+        <translation>Penampilan</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="118"/>
+        <source>Corner Radius</source>
+        <translation>Jejari Bucu</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="108"/>
+        <source>Openbox Theme</source>
+        <translation>Tema Openbox</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation>Kelakuan</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation>Tetikus &amp; Pad Sentuh</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation>Bebayang Jatuh</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation>Dasar Penempatan</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
+        <source>Cursor Theme</source>
+        <translation>Tema Kursor</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="190"/>
+        <source>Cursor Size</source>
+        <translation>Saiz Kursor</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="200"/>
+        <source>Natural Scroll</source>
+        <translation>Tatal Semulajadi</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="241"/>
+        <source>Add</source>
+        <translation>Tambah</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="248"/>
+        <source>Remove</source>
+        <translation>Buang</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="81"/>
+        <source>Language &amp; Region</source>
+        <translation>Bahasa &amp; Wilayah</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="226"/>
+        <source>Keyboard Layout</source>
+        <translation>Susun Atur Papan Kekunci</translation>
+    </message>
+</context>
+</TS>
