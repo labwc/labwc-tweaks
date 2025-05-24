246a52f7b8880129e212a3dae0ef9d107dcaf8ef
Weblate <noreply@weblate.org>
2025-05-24 18:28:17 +0200

    Weblate commits
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: therealmate <hellogaming91@gmail.com>

---
+++ data/translations/labwc-tweaks_hu.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../src/maindialog.ui" line="57"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="118"/>
+        <source>Corner Radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="108"/>
+        <source>Openbox Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
+        <source>Cursor Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="190"/>
+        <source>Cursor Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="200"/>
+        <source>Natural Scroll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="241"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="248"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="81"/>
+        <source>Language &amp; Region</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="226"/>
+        <source>Keyboard Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
