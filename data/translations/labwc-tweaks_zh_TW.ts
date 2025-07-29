77bcfa36e6d7ed303ca7e223f32e2399e3f151fd
Weblate <noreply@weblate.org>
2025-07-29 08:55:13 +0200

    Weblate commits
    Co-authored-by: Abdullah Albaroty <albaroty@gmail.com>
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/ar/
    Translation: Labwc/Labwc Tweaks

--- data/translations/labwc-tweaks_zh_TW.ts
+++ data/translations/labwc-tweaks_zh_TW.ts
@@ -1,77 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ar">
+<TS version="2.1" language="zh_TW">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="57"/>
         <source>Appearance</source>
-        <translation>المظهر</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="118"/>
         <source>Corner Radius</source>
-        <translation>أركان نصف قطرية</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>Openbox Theme</source>
-        <translation>سمة أوبن بوكس</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="65"/>
         <source>Behaviour</source>
-        <translation>السلوك</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="73"/>
         <source>Mouse &amp; Touchpad</source>
-        <translation>الفأرة و لوح اللمس</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="128"/>
         <source>Drop Shadows</source>
-        <translation>أسقط الظلال</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="154"/>
         <source>Placement Policy</source>
-        <translation>سياسة الموضع</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="180"/>
         <source>Cursor Theme</source>
-        <translation>سمة المؤشر</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="190"/>
         <source>Cursor Size</source>
-        <translation>حجم المؤشر</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="200"/>
         <source>Natural Scroll</source>
-        <translation>تمرير طبيعي</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="241"/>
         <source>Add</source>
-        <translation>أضف</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="248"/>
         <source>Remove</source>
-        <translation>احذف</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="81"/>
         <source>Language &amp; Region</source>
-        <translation>اللغة والمنطقة</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="226"/>
         <source>Keyboard Layout</source>
-        <translation>تخطيط لوحة المفاتيح</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
