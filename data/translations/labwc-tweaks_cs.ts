7a2e4d6a6892bdee97691e8f6829743e83ebbcde
Weblate <noreply@weblate.org>
2025-10-12 13:27:38 +0200

    Weblate commits
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/cs/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

---
+++ data/translations/labwc-tweaks_cs.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../src/maindialog.ui" line="57"/>
+        <source>Appearance</source>
+        <translation>Vzhled</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="118"/>
+        <source>Corner Radius</source>
+        <translation>Poloměr rohů</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="108"/>
+        <source>Openbox Theme</source>
+        <translation>Motiv vzhledu Openbox</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation>Chování</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation>Myš a touchpad</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation>Vrhané stíny</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation>Zásada umisťování</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
+        <source>Cursor Theme</source>
+        <translation>Vzhled ukazatele</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="190"/>
+        <source>Cursor Size</source>
+        <translation>Velikost ukazatele</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="200"/>
+        <source>Natural Scroll</source>
+        <translation>Přirozené posouvání</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="241"/>
+        <source>Add</source>
+        <translation>Přidat</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="248"/>
+        <source>Remove</source>
+        <translation>Odebrat</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="81"/>
+        <source>Language &amp; Region</source>
+        <translation>Jazyk a oblast</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="226"/>
+        <source>Keyboard Layout</source>
+        <translation>Rozvržení klávesnice</translation>
+    </message>
+</context>
+</TS>
