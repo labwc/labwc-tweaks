325b661521e3623aeb05a878daeda6cb3e681a4c
Weblate <noreply@weblate.org>
2025-10-30 07:38:34 +0100

    Weblate commits
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Co-authored-by: zenfas <zenfas66@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/he/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/vi/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/he/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/vi/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

---
+++ data/translations/labwc-tweaks_vi.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../src/maindialog.ui" line="57"/>
+        <source>Appearance</source>
+        <translation>Giao diện</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="118"/>
+        <source>Corner Radius</source>
+        <translation>Bán kính góc</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="108"/>
+        <source>Openbox Theme</source>
+        <translation>Chủ đề Openbox</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="65"/>
+        <source>Behaviour</source>
+        <translation>Hành vi</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="73"/>
+        <source>Mouse &amp; Touchpad</source>
+        <translation>Chuột &amp; Bàn di</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="128"/>
+        <source>Drop Shadows</source>
+        <translation>Bóng đổ</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="154"/>
+        <source>Placement Policy</source>
+        <translation>Chính sách đặt cửa sổ</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="180"/>
+        <source>Cursor Theme</source>
+        <translation>Chủ đề con trỏ</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="190"/>
+        <source>Cursor Size</source>
+        <translation>Kích thước con trỏ</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="200"/>
+        <source>Natural Scroll</source>
+        <translation>Cuộn tự nhiên</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="241"/>
+        <source>Add</source>
+        <translation>Thêm</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="248"/>
+        <source>Remove</source>
+        <translation>Xóa</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="81"/>
+        <source>Language &amp; Region</source>
+        <translation>Ngôn ngữ &amp; Vùng</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="226"/>
+        <source>Keyboard Layout</source>
+        <translation>Bố cục bàn phím</translation>
+    </message>
+</context>
+</TS>
