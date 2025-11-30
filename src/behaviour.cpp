ec85b913fe7f819fb3898aaa0aeeb138ceabbdcd
Johan Malm <jgm323@gmail.com>
2025-11-30 20:41:22 +0000

    behaviour.cpp: translate values in Placement Policy QComboBox
    ...by providing both text (the translated stuff) and data (the value
    that we want to go into rc.xml) when calling addItem().
    
    Fixes: #128

--- src/behaviour.cpp
+++ src/behaviour.cpp
@@ -1,6 +1,8 @@
 #include "behaviour.h"
+#include <QVectorIterator>
 #include "find-themes.h"
 #include "macros.h"
+#include "pair.h"
 #include "settings.h"
 #include "./ui_behaviour.h"
 
@@ -17,13 +19,24 @@ Behaviour::~Behaviour()
 void Behaviour::activate()
 {
     /* Placement Policy */
-    QStringList policies = { "", "Automatic", "Cascade", "Center", "Cursor" };
-    ui->placementPolicy->addItems(policies);
-    ui->placementPolicy->setCurrentIndex(
-            policies.indexOf(getStr("/labwc_config/placement/policy")));
+    QVector<QSharedPointer<Pair>> policies;
+    policies.append(QSharedPointer<Pair>(new Pair("automatic", tr("Automatic"))));
+    policies.append(QSharedPointer<Pair>(new Pair("cascade", tr("Cascade"))));
+    policies.append(QSharedPointer<Pair>(new Pair("center", tr("Center"))));
+    policies.append(QSharedPointer<Pair>(new Pair("cursor", tr("Cursor"))));
+
+    QString current = getStr("/labwc_config/placement/policy");
+    int index = -1;
+    foreach (auto policy, policies) {
+        ui->placementPolicy->addItem(policy.get()->description(), QVariant(policy.get()->value()));
+        ++index;
+        if (current == policy.get()->value()) {
+            ui->placementPolicy->setCurrentIndex(index);
+        }
+    }
 }
 
 void Behaviour::onApply()
 {
-    setStr("/labwc_config/placement/policy", TEXT(ui->placementPolicy));
+    setStr("/labwc_config/placement/policy", DATA(ui->placementPolicy));
 }
