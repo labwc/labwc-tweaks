0cd9a24ee6de027a3705a7f5399f74deb573622d
Weblate <noreply@weblate.org>
2025-12-09 20:28:00 +0100

    Weblate commits
    Co-authored-by: Papoteur <yves.brungard_git@gadz.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: therealmate <hellogaming91@gmail.com>
    Co-authored-by: virtual-hand <handvirtual@tutamail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks-menu-entry/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/hu/
    Translate-URL: https://translate.lxqt-project.org/projects/labwc/labwc-tweaks/pt_BR/
    Translation: Labwc/Labwc Tweaks
    Translation: Labwc/Labwc Tweaks Menu Entry

--- data/translations/labwc-tweaks_fr.ts
+++ data/translations/labwc-tweaks_fr.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../../src/behaviour.cpp" line="23"/>
         <source>Automatic</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatique</translation>
     </message>
     <message>
         <location filename="../../src/behaviour.cpp" line="24"/>
         <source>Cascade</source>
-        <translation type="unfinished"></translation>
+        <translation>Cascade</translation>
     </message>
     <message>
         <location filename="../../src/behaviour.cpp" line="25"/>
         <source>Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Centré</translation>
     </message>
     <message>
         <location filename="../../src/behaviour.cpp" line="26"/>
         <source>Cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>Curseur</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
     <message>
         <location filename="../../src/language.cpp" line="28"/>
         <source>Select layout to add...</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélection de la disposition à ajouter...</translation>
     </message>
 </context>
 <context>
@@ -60,12 +60,12 @@
     <message>
         <location filename="../../src/main.cpp" line="52"/>
         <source>Error loading </source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur au chargement </translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="54"/>
         <source>Run labwc-tweaks from a terminal to view error messages</source>
-        <translation type="unfinished"></translation>
+        <translation>Exécutez labwc-tweaks en concole pour voir les messages d&apos;erreur</translation>
     </message>
 </context>
 <context>
@@ -73,7 +73,7 @@
     <message>
         <location filename="../../src/appearance.ui" line="29"/>
         <source>Labwc Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Thème Labwc</translation>
     </message>
     <message>
         <location filename="../../src/appearance.ui" line="39"/>
@@ -88,7 +88,7 @@
     <message>
         <location filename="../../src/appearance.ui" line="70"/>
         <source>Icon Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Thème d&apos;icônes</translation>
     </message>
 </context>
 <context>
@@ -122,12 +122,12 @@
     <message>
         <location filename="../../src/mouse.ui" line="29"/>
         <source>Cursor Theme</source>
-        <translation>Thème curseur</translation>
+        <translation>Thème du curseur</translation>
     </message>
     <message>
         <location filename="../../src/mouse.ui" line="39"/>
         <source>Cursor Size</source>
-        <translation>Taille curseur</translation>
+        <translation>Taille du curseur</translation>
     </message>
     <message>
         <location filename="../../src/mouse.ui" line="49"/>
