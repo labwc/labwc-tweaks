ce3900745045ae043f3fd2ac2f35161db5328991
stefonarch <standreas@riseup.net>
2025-12-17 10:43:40 +0100

    Updated .ts files

--- data/translations/labwc-tweaks_zh_CN.ts
+++ data/translations/labwc-tweaks_zh_CN.ts
@@ -56,6 +56,65 @@
     </message>
 </context>
 <context>
+    <name>Mouse</name>
+    <message>
+        <location filename="../../src/mouse.cpp" line="42"/>
+        <source>Flat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="43"/>
+        <source>Adaptive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="62"/>
+        <source>lrm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="63"/>
+        <source>lmr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="101"/>
+        <location filename="../../src/mouse.cpp" line="122"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="102"/>
+        <source>Button Area</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="103"/>
+        <source>Clickfinger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="120"/>
+        <source>Two Finger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="121"/>
+        <source>Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="141"/>
+        <source>Enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.cpp" line="143"/>
+        <source>Disable with external mouse</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
         <location filename="../../src/main.cpp" line="52"/>
@@ -120,19 +179,155 @@
 <context>
     <name>pageMouse</name>
     <message>
-        <location filename="../../src/mouse.ui" line="29"/>
+        <location filename="../../src/mouse.ui" line="17"/>
         <source>Cursor Theme</source>
         <translation>光标主题</translation>
     </message>
     <message>
-        <location filename="../../src/mouse.ui" line="39"/>
+        <location filename="../../src/mouse.ui" line="27"/>
         <source>Cursor Size</source>
         <translation>光标大小</translation>
     </message>
     <message>
-        <location filename="../../src/mouse.ui" line="49"/>
+        <location filename="../../src/mouse.ui" line="37"/>
         <source>Natural Scroll</source>
         <translation>自然滚动</translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="54"/>
+        <source>Left Handed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="71"/>
+        <source>Pointer Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="94"/>
+        <source>Acceleration Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="102"/>
+        <source>flat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="107"/>
+        <source>adaptive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="115"/>
+        <source>Touchpad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="123"/>
+        <source>enabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="128"/>
+        <source>disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="133"/>
+        <source>disabled-on-external-mouse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="141"/>
+        <source>Tap to Click</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="161"/>
+        <source>Tap Button Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="169"/>
+        <source>lrm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="174"/>
+        <source>lmr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="182"/>
+        <source>Tap and Drag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="212"/>
+        <source>Drag Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="229"/>
+        <source>Three Finger Drag</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="246"/>
+        <source>Middle Button Emulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="263"/>
+        <source>Disable While Typing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="280"/>
+        <source>Click Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="288"/>
+        <location filename="../../src/mouse.ui" line="329"/>
+        <source>none</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="293"/>
+        <source>buttonareas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="298"/>
+        <source>clickfinger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="306"/>
+        <source>Scroll Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="314"/>
+        <source>twofinger</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="319"/>
+        <source>edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="324"/>
+        <source>onbuttondown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/mouse.ui" line="337"/>
+        <source>Scroll Factor</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
