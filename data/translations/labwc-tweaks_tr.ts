694d0b0d326e95168c7b2e034504c8bd112d1702
stefonarch <standreas@riseup.net>
2026-01-28 21:38:51 +0100

    Updated *.ts files

--- data/translations/labwc-tweaks_tr.ts
+++ data/translations/labwc-tweaks_tr.ts
@@ -483,28 +483,33 @@ If disabled, an outlined rectangle is shown</source>
 <context>
     <name>MainDialog</name>
     <message>
-        <location filename="../../src/maindialog.cpp" line="46"/>
+        <location filename="../../src/maindialog.cpp" line="47"/>
         <source>Appearance</source>
         <translation>Görünüş</translation>
     </message>
     <message>
-        <location filename="../../src/maindialog.cpp" line="50"/>
+        <location filename="../../src/maindialog.cpp" line="51"/>
         <source>Behaviour</source>
         <translation>Davranış</translation>
     </message>
     <message>
-        <location filename="../../src/maindialog.cpp" line="54"/>
+        <location filename="../../src/maindialog.cpp" line="55"/>
         <source>Mouse &amp; Touchpad</source>
         <translation>Fare ve Dokunmatik Yüzey</translation>
     </message>
     <message>
-        <location filename="../../src/maindialog.cpp" line="58"/>
+        <location filename="../../src/maindialog.cpp" line="59"/>
         <source>Keyboard</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/maindialog.cpp" line="62"/>
+        <location filename="../../src/maindialog.cpp" line="63"/>
         <source>Touchscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="67"/>
+        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -605,6 +610,55 @@ If disabled, an outlined rectangle is shown</source>
     <message>
         <location filename="../../src/touchscreen.cpp" line="27"/>
         <source>Inverted</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>pageAbout</name>
+    <message>
+        <location filename="../../src/about.ui" line="31"/>
+        <location filename="../../src/about.ui" line="177"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="45"/>
+        <source>XWayland support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="59"/>
+        <source>Native language support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="73"/>
+        <source>SVG icon support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="87"/>
+        <source>Icon support with libsfdo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="101"/>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="119"/>
+        <source>Environment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="191"/>
+        <source>Licenses</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/about.ui" line="205"/>
+        <source>Development</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
